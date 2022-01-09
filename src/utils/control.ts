b2209b4c38cdf3c131bc70bc2ad5f27dfc298d2e
LucasForde <lucasmforde@gmail.com>
2022-01-09 14:57:36 +0000

    changed speed to 100ms/generation

--- src/utils/control.ts
+++ src/utils/control.ts
@@ -3,7 +3,7 @@ import generation from './generation'
 const control = {
   start: function (callBack: (arg: boolean) => void) {
     callBack(true)
-    this.interval = setInterval(generation, 25)
+    this.interval = setInterval(generation, 100)
   },
   stop: function (callBack: (arg: boolean) => void) {
     callBack(false)
